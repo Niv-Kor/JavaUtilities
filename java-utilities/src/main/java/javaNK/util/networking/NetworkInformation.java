da4ccdf31df275b4606d904b6fcf6ab66efe4958
Korach <Korach@Korach-PC>
2019-07-07 02:33:32 +0300

    Check in

--- java-utilities/src/main/java/javaNK/util/networking/NetworkInformation.java
+++ java-utilities/src/main/java/javaNK/util/networking/NetworkInformation.java
@@ -4,44 +4,94 @@ import java.net.InetAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
 
+/**
+ * This class stores information about a network, that's crucial for communication
+ * between two machines. The network does not have to be the Internet network,
+ * but could be rather any other private network as well. 
+ * 
+ * @author Niv Kor
+ */
 public class NetworkInformation
 {
 	private int localPort;
 	private InetAddress address;
 	
+	/**
+	 * @throws IOException when the local host address is unavailable.
+	 */
 	public NetworkInformation() throws IOException {
 		this(PortGenerator.nextPort(), InetAddress.getLocalHost());
 	}
 	
-	public NetworkInformation(Integer port) throws IOException {
+	/**
+	 * @param port - Port number
+	 * @throws IOException when the local host address is unavailable.
+	 */
+	public NetworkInformation(int port) throws IOException {
 		this(port, InetAddress.getLocalHost());
 	}
 	
+	/**
+	 * @param ip - IP address (such as '127.0.0.1')
+	 * @throws IOException when the address is unavailable.
+	 */
 	public NetworkInformation(String ip) throws IOException {
 		this(-1, ip);
 	}
 	
+	/**
+	 * @param address - Logical address
+	 * @throws IOException when the address is unavailable.
+	 */
 	public NetworkInformation(InetAddress address) throws IOException {
 		this(-1, address);
 	}
 	
-	public NetworkInformation(Integer port, String ip) throws IOException {
-		this(port, InetAddress.getByName(ip));
-	}
-	
+	/**
+	 * Construct a NetworkInformation object using a JSON message of type "network_information"
+	 * that other classes in the package "networking" use.
+	 * 
+	 * @param msg - The "network_information" JSON message
+	 * @throws IOException when the address is unavailable.
+	 */
 	public NetworkInformation(JSON msg) throws IOException {
 		this(msg.getInt("port"), msg.getString("ip"));
 	}
 	
-	public NetworkInformation(Integer port, InetAddress address) throws IOException {
+	/**
+	 * @param port - Port number
+	 * @param ip - IP address (such as '127.0.0.1')
+	 * @throws IOException when the address is unavailable.
+	 */
+	public NetworkInformation(int port, String ip) throws IOException {
+		this(port, InetAddress.getByName(ip));
+	}
+	
+	/**
+	 * @param port - Port number
+	 * @param ip - IP address (such as '127.0.0.1')
+	 * @throws IOException when the address is unavailable.
+	 */
+	public NetworkInformation(int port, InetAddress address) throws IOException {
+		if (address == null) throw new IOException();
+		
 		this.address = address;
 		this.localPort = (PortGenerator.isLegal(port)) ? port : PortGenerator.nextPort();
 	}
 	
+	/**
+	 * @return the local port.
+	 */
 	public int getLocalPort() { return localPort; }
 	
+	/**
+	 * @return the IP address.
+	 */
 	public InetAddress getAddress() { return address; }
 	
+	/**
+	 * @return the IP address' name.
+	 */
 	public String getIP() {
 		String addressStr = address.toString();
 		return addressStr.substring(addressStr.indexOf('/') + 1, addressStr.length());
@@ -56,8 +106,14 @@ public class NetworkInformation
 		catch (PortsUnavailableException e) {}
 	}
 	
+	/**
+	 * @param a - The new address
+	 */
 	public void setAddress(InetAddress a) { address = a; }
 	
+	/**
+	 * @param ip - The name of the new address
+	 */
 	public void setAddress(String ip) {
 		try { address = InetAddress.getByName(ip); }
 		catch (UnknownHostException e) {}
@@ -88,6 +144,6 @@ public class NetworkInformation
 	
 	@Override
 	public String toString() {
-		return "[Network IP: " + getIP() + ", Port: " + localPort + "]";
+		return "[IP Address: " + getIP() + ", Port: " + localPort + "]";
 	}
 }
